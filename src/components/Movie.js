666cdf5d0b38c57675248e14d15adf560f240f2e
verndev <vernet0312@gmail.com>
2021-07-19 01:44:53 -0400

    Breadcrumb links on movie details page complete.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,21 +1,21 @@
-import React            from 'react';
-import {IMAGE_BASE_URL, POSTER_SIZE} from "../config";
-import Grid    from "./Grid";
+import React         from 'react';
 import Spinner       from "./Spinner";
+import BreadCrumb    from "./BreadCrumb";
 import useMovieFetch from "../hooks/useMovieFetch";
-import NoImage from '../images/no_image.jpg';
-import {useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 
 const Movie = () => {
 	const {movieId} = useParams();
-	const{state:movie, loading, error} = useMovieFetch(movieId)
+	const {state: movie, loading, error} = useMovieFetch(movieId)
 
-	console.log(movie);
+	if (loading) return <Spinner/>;
+
+	if (error) return <div>Something went wrong!...</div>
 
 	return (
 		<>
-			<div> Movie </div>
+			< BreadCrumb movieTitle={movie.original_title}/>
 		</>
 	);
 };
