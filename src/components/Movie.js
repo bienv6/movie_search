910d57245b1f8617efe61f0fced7e79157a44721
verndev <vernet0312@gmail.com>
2021-07-20 15:17:04 -0400

    Movie actors component finished.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,14 @@
-import React         from 'react';
-import Spinner    from "./Spinner";
-import MovieInfo  from "./MovieInfo";
-import BreadCrumb    from "./BreadCrumb";
-import MovieInfoBar  from "./MovieInfoBar";
-import useMovieFetch from "../hooks/useMovieFetch";
-import { useParams } from 'react-router-dom';
+import React           from 'react';
+import Spinner         from "./Spinner";
+import MovieInfo       from "./MovieInfo";
+import BreadCrumb      from "./BreadCrumb";
+import MovieInfoBar    from "./MovieInfoBar";
+import useMovieFetch   from "../hooks/useMovieFetch";
+import { useParams }   from 'react-router-dom';
+import Grid            from "./Grid";
+import Actor           from "./Actor";
+import NoImage         from "../images/no_image.jpg"
+import { POSTER_SIZE, IMAGE_BASE_URL } from "../config";
 
 
 const Movie = () => {
@@ -20,8 +24,20 @@ const Movie = () => {
 			< BreadCrumb movieTitle={movie.original_title}/>
 			< MovieInfo movie={movie}/>
 			<MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue}/>
+			<Grid header='Actors'>
+				{
+					movie.actors.map(actor => (
+						<Actor key={actor.credit_id} name={actor.name} character={actor.character}
+						       imageUrl={actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+							       : NoImage}/>
+
+					))
+				}
+
+			</Grid>
 		</>
 	);
-};
+}
+;
 
 export default Movie;
